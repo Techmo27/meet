cde6b93a88ccc6a722d6b1d246859bf25b037eea
Techmo27 <e.vins@yahoo.com>
2022-03-09 11:27:05 +0100

    Updated Atatus

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import * as atatus from 'atatus-spa';
 
+import * as atatus from 'atatus-spa';
 atatus.config('c6fa3e3c42ed455f85317845850ccf1f').install();
 
 ReactDOM.render(
