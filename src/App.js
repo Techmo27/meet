a3870a0d91ee2f754b3e6e3834cef3ece864287c
Techmo27 <e.vins@yahoo.com>
2022-03-16 11:56:13 +0100

    Added Scatter Chart

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from "./NumberOfEvents";
-
+import { OfflineAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
-import { getEvents, extractLocations, checkToken, getAccessToken } from
-  './api';
-
+import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 import './App.css';
 import './nprogress.css';
@@ -34,6 +33,15 @@ class App extends Component {
         if (this.mounted) {
           this.setState({ events, locations: extractLocations(events) });
         }
+      });
+    }
+    if (!navigator.onLine) {
+      this.setState({
+        alertText: 'Data is cached from previous visit and is not uptodate'
+      });
+    } else {
+      this.setState({
+        alertText: ''
       });
     }
   }
@@ -69,12 +77,25 @@ class App extends Component {
     this.updateEvents(this.state.currentLocation);
   };
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number };
+    })
+    return data;
+  };
+
   render() {
     if (this.state.showWelcomeScreen === undefined) return <div className="App" />
+
     return (
       <div className="App">
         <h1>Meet App</h1>
         <h3>Choose a city</h3>
+        <OfflineAlert text={this.state.alertText}
+        />
         <CitySearch
           locations={this.state.locations}
           updateEvents={this.updateEvents}
@@ -83,6 +104,14 @@ class App extends Component {
           updateEvents={this.updateEvents}
           numberOfEvents={this.state.numberOfEvents}
         />
+        <ScatterChart width={730} height={250}
+          margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis type="category" dataKey="city" name="city" />
+          <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
         <EventList
           events={this.state.events}
         />
